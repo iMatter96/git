8763e45e6fbf81742186aac8295209b941095080
dukai <dukaiv@qq.com>
2021-04-29 10:26:22 +0800

    second commit

--- src/main/java/com/yuanqi/GitTest.java
+++ src/main/java/com/yuanqi/GitTest.java
@@ -9,5 +9,6 @@ package com.yuanqi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1");
+        System.out.println("hello git 2");
     }
 }
