126f9981e4cc3e0735d2bae04705920f51a1e930
dukai <dukaiv@qq.com>
2021-04-29 10:16:24 +0800

    first commit

---
+++ src/main/java/com/yuanqi/GitTest.java
@@ -0,0 +1,13 @@
+package com.yuanqi;
+
+/**
+ * @Auther:dukai
+ * @Date:2021-04-29-04-29-10:15
+ * @Description:com.yuanqi
+ * @version:1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git 1");
+    }
+}
